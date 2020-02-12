03075f763563409efaab36d4cb46ef4ff2455589
Nahom Asrat <nahomcookies@gmail.com>
2020-02-12 17:15:27 +0300

    changes were made but i wish i remembered what they were

--- App.js
+++ App.js
@@ -2,14 +2,15 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState();
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
 
   const goalInputHandler = (enteredText) => {
     setEnteredGoal(enteredText);
   };
 
   const addGoalHandler = () => {
-    console.log(enteredGoal);
+    setCourseGoals(currentGoals => [...currentGoals,enteredGoal]);
   };
 
   return (
@@ -19,10 +20,12 @@ export default function App() {
         placeholder='Course Goal'
         style={styles.input}
         onChangeText={goalInputHandler}
-        value={enteredGoal}
+        value={enteredGoal}/>
 
-        />
         <Button title='ADD' onPress={addGoalHandler}/>
+      </View>
+      <View>
+        {courseGoals.map((goal) => <View key={goal} style={styles.listItem}><Text>{goal}</Text></View>)}
       </View>
     </View>
   );
@@ -42,5 +45,12 @@ const styles = StyleSheet.create({
     borderColor: 'grey',
     borderBottomWidth: 2,
     padding: 10
+  },
+  listItem:{
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
   }
 });
